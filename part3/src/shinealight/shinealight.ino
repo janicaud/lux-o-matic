aff149a08dcde12d763b2bb4ca096f0d75b35015
Nicolas Janicaud <janicaud@gmail.com>
2023-02-16 08:08:10 +0100

    Last up

--- part3/src/shinealight/shinealight.ino
+++ part3/src/shinealight/shinealight.ino
@@ -3,6 +3,10 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+
+// in setup()
 
 #define PIN 5
 
@@ -14,17 +18,27 @@ Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(32, 8, PIN,
 
 void setup() {
   matrix.begin();
+  Serial.begin(115200);
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
 }
 
 #define LED_RED_HIGH 		(31 << 11)
 
-int position = 0;
+int line = 0;
+int column = 0;
 
 void loop() {
-  matrix.fillScreen(0);
-  if(position > 32)
-    position= 0;
-  matrix.drawPixel(1,2 + position++,LED_RED_HIGH);
+  matrix.clear();
+  if(line > 8)
+  {
+    line= 0;
+    column++;
+    if(column > 32)
+      column=0;
+
+  }
+  matrix.drawPixel(column,line++,matrix.Color(255, 0, 0));
   matrix.show();
+  Serial.print(".");
   delay(100);
 }
